0846f8e368b08bb8661a2d55732ee0a07ffa4522
TTT <teemu.t.toivonen@tuni.fi>
2026-01-28 22:18:58 +0200

    eka committi testihaara2

--- testitiedosto.py
+++ testitiedosto.py
@@ -1,7 +1,8 @@
-#Tämä on toinen committi jee
+#Tämä on testihaara2 tehty eka committi
 
 def main():
     print("Testiohjelma")
     print("uusi rivi jee")
+    print("taman pitaisi olla haarassa testihaara2")
 
 main
